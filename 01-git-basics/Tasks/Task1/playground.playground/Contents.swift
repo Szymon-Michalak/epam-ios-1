7f01fb3660d29f8b9cc0c907dd1db8e3c257e4b5
Szymon Michalak <sz.p.michalak@gmail.com>
2025-05-25 15:18:36 +0100

    Add Hello-World program

---
+++ 01-git-basics/Tasks/Task1/playground.playground/Contents.swift
@@ -0,0 +1,8 @@
+// This is a comment, ignored by the compiler
+
+print("Hello World!") // You can also write a comment next to an instruction
+
+/* This is also a comment,
+which spans multiple lines
+and ends only with a
+closing delimiter */
